2d414b9e5eaf98cf7a6ab6d8761376d7e70baa5c
obetta1 <obettabenjamin44@gmail.com>
2021-08-20 22:38:04 +0100

    start

--- app/src/main/java/com/example/tictaktoe/MainActivity.kt
+++ app/src/main/java/com/example/tictaktoe/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.tictaktoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var enter_button:ImageView = findViewById(R.id.enter_button)
+        var text :TextView = findViewById(R.id.textView)
+
+        enter_button.setOnClickListener {
+            text.text = "Benjmain"
+
+        }
     }
 }
